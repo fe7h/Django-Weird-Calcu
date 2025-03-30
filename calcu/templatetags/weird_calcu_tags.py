9d427474e96c9b9e2ca8d47ea6342a85ba2f5da9
fesh <189043093+fe7h@users.noreply.github.com>
2025-03-30 18:42:48 +0300

    created template tags

---
+++ calcu/templatetags/weird_calcu_tags.py
@@ -0,0 +1,15 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+from calcu.forms import WeirdCalcuForm
+
+register = template.Library()
+
+
+@register.inclusion_tag('calcu/calcu_form.html')
+def weird_calcu_form():
+    return {'form': WeirdCalcuForm}
+
+@register.simple_tag
+def weird_calcu_answer():
+    return mark_safe('<div id="div"></div>')
